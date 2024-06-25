da6fca53b5623a9d2c5d4a7aa98d8cd6972b0e25
Section1 <otinoco@digiocio.com>
2024-06-26 00:49:20 +0200

    Codi 2 - Transformant de veu a text amb el model d'entrenament en Català

--- index.py
+++ index.py
@@ -1,1 +1,23 @@
-print("Hola classe")
+import speech_recognition as sr
+import json
+
+def listen():
+    rec = sr.Recognizer()
+    with sr.Microphone() as src:
+        rec.adjust_for_ambient_noise(src)
+        audio = rec.listen(src)
+    try:
+        cmd = rec.recognize_vosk(audio)
+    except Exception:
+        print("No puc entendre't. Intenta escriure-ho?")
+        cmd = input()
+    return cmd
+
+def listenToText():
+    c = listen()
+    print("Valor escoltat=",c)
+    d = json.loads(c)
+    return str(d["text"])
+
+print(listenToText())
+print("Hola Classe que tal?")
