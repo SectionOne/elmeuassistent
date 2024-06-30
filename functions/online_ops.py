bc24823ecdd16c9e1e1df75fce7bc15c7bafab36
Section1 <otinoco@digiocio.com>
2024-06-30 12:01:55 +0200

    Codi 22 - Funció explicar acudits

--- functions/online_ops.py
+++ functions/online_ops.py
@@ -1,4 +1,5 @@
 import pywhatkit as kit
+import requests
 
 def search_on_google(query):
     kit.search(query)
@@ -8,3 +9,17 @@ def play_on_youtube(video):
     
 def send_whatsapp_message(number, message):
     kit.sendwhatmsg_instantly(f"+34{number}", message)
+    
+def get_random_joke():
+    response = {}
+    headers = {
+           'Accept': 'application/json'
+    }
+    res = requests.get("https://v2.jokeapi.dev/joke/Any?lang=es", headers=headers).json()
+    if res["type"] == "single":
+        response["joke"] = res["joke"]
+        response["answer"] = ""
+    else:
+        response["joke"] = res["setup"]
+        response["answer"] = res["delivery"]
+    return response
